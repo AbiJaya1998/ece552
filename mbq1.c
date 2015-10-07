cc64e3242b822b70176a3559c2433d0410f49533
Von Zuben  Christopher Leonard <vonzube2@ug239.eecg>
2015-10-07 16:08:22 -0400

    modified mbq1 for better case coverage in q1 and q2

--- mbq1.c
+++ mbq1.c
@@ -1,18 +1,16 @@
 
-
-
 int main (int argc, char** argv) {
 
-    int a = 0, b = 0, c = 0;
+    register unsigned int a = 0, b = 0, c = 0;
 
     int j;
-    for ( j = 0; j < 10000000; ++j ){
+    for ( j = 0; j < 1000000; ++j ){
 
-        a += 1;
+        a = 1;
 
-        b += a;
+        b = 1;
 
-        c += a + b;
+        c = a + b;
 
     }
 
