88d1c0c7a47980b73fd97e3d6042e43b5aec03dc
AldrichW <aldrich.wingsiong@mail.utoronto.ca>
2015-11-02 13:15:33 -0500

    [Lab3] Adding lab3 code

---
+++ lab3/simplesim-3.0d-assig3/RunEIOTraces.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sim-safe -max:inst 1000000 /cad2/ece552f/benchmarks/gcc.eio
+sim-safe -max:inst 1000000 /cad2/ece552f/benchmarks/go.eio
+sim-safe -max:inst 1000000 /cad2/ece552f/benchmarks/compress.eio
+
