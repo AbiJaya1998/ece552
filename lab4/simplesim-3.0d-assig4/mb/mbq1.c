8e8bfbc5ddc2723e99ce45da61c39654e85c54d1
Von Zuben  Christopher Leonard <vonzube2@ug137.eecg>
2015-11-17 21:45:27 -0500

    updated mbq1

--- lab4/simplesim-3.0d-assig4/mb/mbq1.c
+++ lab4/simplesim-3.0d-assig4/mb/mbq1.c
@@ -1,7 +1,7 @@
 
 #include<stdlib.h>
 
-#define SIZE 10000
+#define SIZE 1000000
 
 int main () {
 
@@ -9,7 +9,13 @@ int main () {
 
     int* mem = (int*)malloc(sizeof(int) * SIZE);
 
+    // this should generate a lot of hits for nextline prefetch
     for ( i = 0; i < SIZE; i++) {
+        mem[i] = 0;
+    }
+
+    // this should skip the cache lines so the nextline prefetch will be useless
+    for ( i = 0; i < SIZE; i+=20) {
         mem[i] = 0;
     }
 
