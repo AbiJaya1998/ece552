fba669af3cf21d66a90fde0895e89f43a064850d
Von Zuben  Christopher Leonard <vonzube2@ug155.eecg>
2015-11-27 11:23:36 -0500

    [lab4] Annotate mbq1.c

--- lab4/simplesim-3.0d-assig4/mb/mbq1.c
+++ lab4/simplesim-3.0d-assig4/mb/mbq1.c
@@ -15,9 +15,20 @@ int main () {
     }
 
     // this should skip the cache lines so the nextline prefetch will be useless
-    for ( i = 0; i < SIZE; i+=20) {
+    for ( i = 0; i < SIZE; i+=32) {
         mem[i] = 0;
     }
+
+    // the first loop should generate about 1000000 hits because
+    // only the first one should be a miss. This will can be confirmed
+    // by looking at the number of hits with this prefetcher
+    //
+    // The second loop should do accesses from different cache lines that are not beside each other
+    // this means it should generate. This is because the i7 processor should have a cache line size
+    // which can hold 16 ints. So a stride that jumps 2 cache lines will generate 1000000 / 2 misses.
+    //
+    // The SIZE variable can be change to see that running the benchmark with different values generates
+    // the expected relative differences
 
     return 0;
 }
