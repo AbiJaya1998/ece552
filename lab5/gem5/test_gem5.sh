9f30ff76504939c5490213f3fb5585e36964645d
Von Zuben  Christopher Leonard <vonzube2@ug242.eecg>
2015-12-11 14:17:08 -0500

    [lab 5] update tester script

--- lab5/gem5/test_gem5.sh
+++ lab5/gem5/test_gem5.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./build/ALPHA/gem5.opt ./configs/example/ruby_random_test.py
+./build/ALPHA/gem5.opt --debug-flags=ProtocolTrace,RubySlicc ./configs/example/ruby_random_test.py -n 2 --num-dirs 1 -m 100000
